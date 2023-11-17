afe889dbab61828bd5a50fe5abfdb5f836bad0b9
Marcello Queiroz Filho <62467354+Mquefi@users.noreply.github.com>
2023-11-17 17:28:57 -0300

    Delete forms directory

--- forms/Aprovadores-secao/inputs.js
+++
@@ -1,6 +0,0 @@
-function preenhe_desc() {
-  $("#hid_desc_reg").val("");
-  var txt = "Seção: " + $("#S0_txt_cod").val() + " - " + $("#S0_txt_desc").val();
-  console.log(txt);
-  $("#hid_desc_reg").val(txt);
-}
